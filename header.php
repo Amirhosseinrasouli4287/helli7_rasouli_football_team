50ad25694fbef0906681662aa8a6ceff98c4fa86
Amirhosseinrasouli4287 <amir.rasoli4287@gmail.com>
2025-03-22 14:04:42 +0330

    درست کردن تایتل و زبان

--- header.php
+++ header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<h lang="en">
+<h lang="fa">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>باشگاه فوتبال</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
-</body>
+
     
