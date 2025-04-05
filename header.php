e3c31e83e203ec13cdbe78c85c9ee51764486509
Amirhosseinrasouli4287 <amir.rasoli4287@gmail.com>
2025-04-05 08:43:14 +0330

    کامل کردن هدر

--- header.php
+++ header.php
@@ -1,16 +1,35 @@
 <!DOCTYPE html>
-<h lang="fa">
+<html lang="fa">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Football Club</title>
+  <title>تیم فوتبال</title>
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-        <ul>
-            <li><a href="#">Team</a></li>
+  <header class="header">
+    <div class="navbar">
+      <img src="logot.png" alt="Logo" class="logo">
+      <nav>
+        <ul class="nav-links">
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">GALLERY</a></li>
+          <li><a href="#">TEAM</a></li>
+          <li><a href="#">FIXTURES</a></li>
+          <li><a href="#">RESULTS</a></li>
+          <li><a href="#">SHOP</a></li>
         </ul>
-    </nav>
-</header>
-
-    
+      </nav>
+      <div class="social">
+        <a href="#">📘</a>
+        <a href="#">🐦</a>
+        <a href="#">🟢</a>
+      </div>
+    </div>
+    <div class="hero">
+      <h1>One Team. One Dream</h1>
+      <p>This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.</p>
+      <a href="#" class="btn">READ MORE ➤</a>
+    </div>
+  </header>
