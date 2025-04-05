94b387cd1d974cb37df7680cc0b85ee2d1e3ad0f
Amirhosseinrasouli4287 <amir.rasoli4287@gmail.com>
2025-04-05 08:22:17 +0330

    پاک کردن و مرتب کردن ابتدای اچ تی ام ال

--- header.php
+++ header.php
@@ -1,23 +1,13 @@
 <!DOCTYPE html>
 <h lang="fa">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>باشگاه فوتبال</title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Football Club</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<header class="main-header">
-    <div class="red-logo">
-        <img src="logot.png">
-    </div>
-
-    <h2>Football Team</h2>
-    <nav>
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Gallery</a></li>
             <li><a href="#">Team</a></li>
         </ul>
     </nav>
